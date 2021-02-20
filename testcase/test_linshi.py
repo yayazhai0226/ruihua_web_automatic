1c592297d5f59fd2374085a0c3e0db049b6ba718
yayazhai0226 <845671607@qq.com>
2021-02-20 17:03:26 +0800

    dd

--- testcase/test_linshi.py
+++ testcase/test_linshi.py
@@ -1,38 +1,49 @@
-from selenium import webdriver
+import sys
+sys.path.append('..')
 from time import sleep
+from selenium import webdriver
+from utils.seleniumtools import find_element , assert_element_exist , hover
 
-url = 'http://192.168.10.126:8040/#/login'
+url = 'http://192.168.10.126:8040/#/login'#地址
+driver = webdriver.Chrome(executable_path='../common/chromedriver.exe')#获取句柄
+driver.get(url)#访问
+driver.maximize_window()#最大化窗口
 
-#创打开浏览器
-driver = webdriver.Chrome(executable_path ="../common/chromedriver.exe")
-#隐式等待：对全局进行控制
-driver.implicitly_wait(6)
-
-#访问
-driver.get(url)
-#刷新
-driver.refresh()
-#最大化浏览器
-driver.maximize_window()
-#输入账号密码
-driver.find_element_by_name('accountName').send_keys('admin')
-driver.find_element_by_name('password').send_keys('000000')
-#点击登录
-driver.find_element_by_id('login').click()
-#进入模块
-driver.find_element_by_xpath('/html/body/app-root/app-home/div/app-sidebar-menu/div/nav/ul/app-menu-item[1]/li/a/div').click()
-#进入子模块
-driver.find_element_by_xpath('/html/body/app-root/app-home/div/app-sidebar-menu/div/nav/ul/app-menu-item[1]/li/ul/li[3]/app-menu-item/li/a').click()
+se_username = ('name','accountName')    #账户
+se_password = ('name','password')       #密码
+cl_denglu = ('id','login')              #登录
+cl_basedata = ('xpath','/html/body/app-root/app-home/div/app-sidebar-menu/div/nav/ul/app-menu-item[1]/li/a')#基础数据
+cl_repository = ('link text','库房管理')#库房管理
+cl_add = ('xpath','/html/body/app-root/app-home/div/main/div/app-basic/app-storehouse/form/span/p-button[1]/button/span[2]')#新增
+cl_add_window = ('class name','ui-dialog-content ui-widget-content')#新增窗口
+se_repo_code = ('class name','ui-grid-col-8')
+se_repo_name = ('xpath','/html/body/app-root/app-home/div/main/div/app-basic/app-storehouse/p-dialog[1]/div/div[2]/form/div[2]/div[2]/input')
+cl_repo_grade = ('xpath','/html/body/app-root/app-home/div/main/div/app-basic/app-storehouse/p-dialog[1]/div/div[2]/form/div[3]/div[2]/p-dropdown/div/label')
+cl_repo_grade_child = ('xpath','/html/body/app-root/app-home/div/main/div/app-basic/app-storehouse/p-dialog[1]/div/div[2]/form/div[3]/div[2]/p-dropdown/div/div[3]/div/ul/li[1]')
+se_repo_code = ('xpath','')
+se_repo_code = ('xpath','')
 
 
 
 
 
-#固定等待
-sleep(6)
-#返回上一页
-# driver.back()
-#切换到下一页
-# driver.forward()
-#退出
-driver.quit()
+find_element(driver , se_username).send_keys('admin')#账户
+find_element(driver , se_password).send_keys('000000')#密码
+find_element(driver , cl_denglu).click()#登录
+find_element(driver , cl_basedata).click()#基础数据
+#hover(cl_basedata)#鼠标悬停
+find_element(driver , cl_repository).click()#仓库管理
+find_element(driver , cl_add).click()#新增
+find_element(driver , se_repo_code).send_keys('000000')#编码
+find_element(driver , se_repo_name).send_keys('000000')#名称
+find_element(driver , cl_repo_grade).click()#级别
+find_element(driver , cl_repo_grade_child).click()#级别下拉
+
+
+
+
+
+
+
+sleep(60)
+#driver.quit()
--- utils/seleniumtools.py
+++ utils/seleniumtools.py
@@ -21,7 +21,7 @@ def find_element(driver , locator , timeout=10):
     '''
     return WebDriverWait(driver , 10).until(lambda s: s.find_element(*locator))
 
-def assert_element_exist(driver , locator , timeout=10):
+def assert_element_exist(locator , timeout=10):
     '''
         方法名：判断元素是否存在
         参数:
@@ -45,3 +45,28 @@ def assert_element_exist(driver , locator , timeout=10):
         return True
     except:
         return False
+
+def hover(locator):
+    '''
+        方法名：鼠标悬停
+        参数:
+            driver:浏览器的句柄
+            locator:元素的定位方式,格式:
+                （'id','xxxx'）
+                （'xpath','xxxx'）
+                （'name','xxxx'）
+                （'class name','xxxx'）
+                （'css selector','xxxx'）
+                （'link text','xxxx'）
+                （'partial link text','xxxx'）
+                （'id','xxxx'）
+        返回值：
+            -找到元素：True
+            -没找到元素：False
+    '''
+    try:
+        element = find_element(locator)
+        ActionChains(driver).move_to_element(element).perform()
+        print('找到元素并悬停')    
+    except:
+        print('未找到元素')    
